5487b9ca026b9d6623b6fdeb6de6a4c3d23884fd
Krystal E. Duran <krystal.e.garcia@gmail.com>
2020-07-08 21:00:10 -0500

    created role table and primary id for roles

--- sql/dataSeed.sql
+++ sql/dataSeed.sql
@@ -7,7 +7,13 @@ CREATE DATABASE employee_db;
 USE employee_db;
 -- Creates the table "department" within employee_db --
 CREATE TABLE department (
-  id integer(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  id INTEGER(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
   -- Makes a string column called "name" which cannot contain null --
   name VARCHAR(30) NOT NULL
+);
+
+-- Creates the table "role" within employee_db --
+CREATE TABLE role (
+    id INTEGER(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    
 );
