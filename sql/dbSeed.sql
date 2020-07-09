276884554653bf031055b71bcbad58e3f8260758
Krystal E. Duran <krystal.e.garcia@gmail.com>
2020-07-08 21:38:46 -0500

    modified integer variables throughout dbSeed.sql to include character limit parameter

--- sql/dbSeed.sql
+++ sql/dbSeed.sql
@@ -17,7 +17,7 @@ CREATE TABLE role (
     id INTEGER(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(10,2) NOT NULL,
-    deparment_id INTEGER,
+    deparment_id INTEGER (10),
     FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
@@ -26,8 +26,8 @@ CREATE TABLE employee (
     id INTEGER(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INTEGER,
+    role_id INTEGER (10),
     FOREIGN KEY (role_id) REFERENCES role(id),
-    manager_id INTEGER,
+    manager_id INTEGER(10),
     FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
