046549c03dc7f68d380959f123c754fe707b1807
Krystal E. Duran <krystal.e.garcia@gmail.com>
2020-07-11 12:04:00 -0500

    added view all data query into queries file

---
+++ sql/queries.sql
@@ -0,0 +1,15 @@
+-- query to view all employee data
+USE employee_db;
+
+SELECT 
+employee.id, 
+employee.first_name,
+employee.last_name,
+role.title,
+department.name,
+role.salary,
+employee.manager_id
+
+FROM employee
+LEFT JOIN role ON employee.role_id = role.id
+LEFT JOIN department ON role.id = department.id;
