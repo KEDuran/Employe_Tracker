5fb35a3d7fd9fc0afe76a397b210cdc0b304b2dd
Krystal E. Duran <krystal.e.garcia@gmail.com>
2020-07-10 21:06:14 -0500

    added intro question variable to kick-off prompting

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 const inquirer = require("inquirer");
+
+const intro = [
+	{
+		type: "list",
+		name: "intro",
+		message: "What would you like to do?",
+		choices: [
+			"View all employees",
+			"View all departments",
+			"View all roles",
+			"Add an employee",
+			"Add a new deparmenet",
+			"Add a new employee role",
+			"Update employee's role",
+			"Update employee's manager",
+			"Update employee's department",
+		],
+	},
+];
