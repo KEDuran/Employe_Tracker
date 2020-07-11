a95ec4e1039800dec6122f81d178a3c2622f9980
Krystal E. Duran <krystal.e.garcia@gmail.com>
2020-07-11 12:55:00 -0500

    created a stored procedure for view all employee data query

--- sql/queries.sql
+++ sql/queries.sql
@@ -2,6 +2,8 @@
 USE employee_db;
 
 -- query to view all employee table data
+CREATE PROCEDURE view_all_employees
+AS
 SELECT 
 employee.id, 
 employee.first_name,
@@ -14,7 +16,8 @@ CONCAT(a.first_name, " ", a.last_name) AS manager
 FROM employee
 LEFT JOIN role ON employee.role_id = role.id
 LEFT JOIN department ON role.id = department.id
-LEFT JOIN employee a ON a.id = employee.manager_id;
+LEFT JOIN employee a ON a.id = employee.manager_id
+GO;
 
 -- query to select all department table data
 
