c2baae3c27b866ffb54f2a5a1ebbbb7df4db0e37
Krystal E. Duran <krystal.e.garcia@gmail.com>
2020-07-11 12:09:10 -0500

    added query to view all department data

--- sql/queries.sql
+++ sql/queries.sql
@@ -13,3 +13,8 @@ employee.manager_id
 FROM employee
 LEFT JOIN role ON employee.role_id = role.id
 LEFT JOIN department ON role.id = department.id;
+
+-- query to select all department data
+USE employee_db;
+
+SELECT * FROM department;
