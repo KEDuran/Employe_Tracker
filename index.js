4a70c98a8d6202541e542cf326d2ce4b7b2c8cd7
Krystal E. Duran <krystal.e.garcia@gmail.com>
2020-07-11 13:11:41 -0500

    removed path references - not needed at this time

--- index.js
+++ index.js
@@ -4,10 +4,6 @@ const dbConnection = require("./sql/dbConnection.js");
 const fs = require("fs");
 const path = require("path");
 
-const SQL_DIR = path.resolve(__dirname, "sql");
-const dbSeedPath = path.join(SQL_DIR, "dbSeed.sql");
-const queriesPath = path.join(SQL_DIR, "queries.sql");
-
 // Function to validaate that each questions is entered.
 function validation() {
 	if (value != "") {
